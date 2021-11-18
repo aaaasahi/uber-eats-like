ca4ee091f28317977224037e1d285785c09acd50
asahi <asap1017@docomo.ne.jp>
2021-11-18 14:39:54 +0900

    line food

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_053731) do
+ActiveRecord::Schema.define(version: 2021_11_18_053831) do
 
   create_table "foods", force: :cascade do |t|
     t.integer "restaurant_id", null: false
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2021_11_18_053731) do
     t.index ["restaurant_id"], name: "index_foods_on_restaurant_id"
   end
 
+  create_table "line_foods", force: :cascade do |t|
+    t.integer "food_id", null: false
+    t.integer "restaurant_id", null: false
+    t.integer "order_id"
+    t.integer "count", default: 0, null: false
+    t.boolean "active", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_line_foods_on_food_id"
+    t.index ["order_id"], name: "index_line_foods_on_order_id"
+    t.index ["restaurant_id"], name: "index_line_foods_on_restaurant_id"
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name", null: false
     t.integer "fee", default: 0, null: false
@@ -31,4 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_053731) do
   end
 
   add_foreign_key "foods", "restaurants"
+  add_foreign_key "line_foods", "foods"
+  add_foreign_key "line_foods", "orders"
+  add_foreign_key "line_foods", "restaurants"
 end
