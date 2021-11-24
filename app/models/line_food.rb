6f818bf546ea56bd8fda9680e0315418c64fc1e4
asahi <asap1017@docomo.ne.jp>
2021-11-24 18:26:09 +0900

    scope

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -2,4 +2,7 @@ class LineFood < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
   belongs_to :order, optional: true
+
+  scope :active, -> { where(active: true) }
+  scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
 end
