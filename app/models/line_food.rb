9c9ab9abc165dc5a513907ebba2c3f9c67c8d5fa
asahi <asap1017@docomo.ne.jp>
2021-11-24 18:28:13 +0900

    合計値

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -3,6 +3,12 @@ class LineFood < ApplicationRecord
   belongs_to :restaurant
   belongs_to :order, optional: true
 
+  validates :count, numericality: { greater_than: 0 }
+
   scope :active, -> { where(active: true) }
   scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
+
+  def total_amount
+    food.price * count
+  end
 end
