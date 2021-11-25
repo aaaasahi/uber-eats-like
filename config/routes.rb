b2c22b392aa9908105cce07776cec67bc61b87ee
asahi <asap1017@docomo.ne.jp>
2021-11-25 09:22:40 +0900

    order

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       end
       resources :line_foods, only: %i[index create]
       put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
     end
   end
 end
