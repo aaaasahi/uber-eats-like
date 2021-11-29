0ff937abbc498fb3a5e3ee65b4d5d7d33ba0c983
asahi <asap1017@docomo.ne.jp>
2021-11-29 21:29:15 +0900

    例外処理

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -3,7 +3,14 @@ module Api
     class LineFoodsController < ApplicationController
       before_action :set_food, only: %i[create]
 
-      
+      def create
+        if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
+          return render json: {
+            existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
+            new_restaurant: Food.find(params[:food_id]).restaurant.name,
+          }, status: :not_acceptable
+        end
+      end
 
       private
       def set_food
