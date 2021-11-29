4b594174c989c5d84b181d79c1ef128226a4325e
asahi <asap1017@docomo.ne.jp>
2021-11-29 21:31:06 +0900

    DBに保存

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -12,6 +12,17 @@ module Api
         end
       end
 
+      set_line_food(@ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
       private
       def set_food
         @ordered_food = Food.find(params[:food_id])
@@ -32,7 +43,7 @@ module Api
           )
         end
       end
-      
+
     end
   end
 end
